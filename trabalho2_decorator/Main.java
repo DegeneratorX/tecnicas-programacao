914d0f5e2817c94461731c411c7bb7db3b768017
Victor <victorio.deg@gmail.com>
2023-06-06 07:59:14 -0300

    Ola

--- trabalho2_decorator/Main.java
+++ trabalho2_decorator/Main.java
@@ -1,6 +1,7 @@
 // Aluno: Victor Medeiros Martins
 // Matrícula: 401339
 
+// Pacote da minha pasta
 package trabalho2_decorator;
 
 // Interface
@@ -8,13 +9,15 @@ interface Mesa {
     String decorar();
 }
 
+
 // Classe concreta que implementa a interface
 class MesaSimples implements Mesa {
     @Override
     public String decorar() {
-        return "Mesa básica";
+        return "Mesa simples";
     }
 }
+
 
 // Decorator abstrato que implementa a interface
 abstract class Decorator implements Mesa {
@@ -37,9 +40,10 @@ class MesaComRodas extends Decorator {
 
     @Override
     public String decorar() {
-        return super.decorar() + ", com rodas";
+        return super.decorar() + "e com rodas";
     }
 }
+
 
 // Decorator concreto para adicionar toalha
 class MesaComToalha extends Decorator {
@@ -49,9 +53,10 @@ class MesaComToalha extends Decorator {
 
     @Override
     public String decorar() {
-        return super.decorar() + ", com toalha";
+        return super.decorar() + "e com toalha";
     }
 }
+
 
 // Decorator concreto para adicionar rede de ping-pong
 class MesaComRedePingPong extends Decorator {
@@ -61,13 +66,13 @@ class MesaComRedePingPong extends Decorator {
 
     @Override
     public String decorar() {
-        return super.decorar() + ", com rede de ping-pong";
+        return super.decorar() + "e com rede de ping-pong";
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        // Cria uma mesa simples
+        // Cria uma mesa simples inicial, que vai servir de base para decoração
         Mesa mesa = new MesaSimples();
         System.out.println(mesa.decorar());
 
@@ -80,6 +85,12 @@ public class Main {
         System.out.println(mesa.decorar());
 
         // Decora a mesa com rede de ping-pong
+        mesa = new MesaComRedePingPong(mesa);
+        System.out.println(mesa.decorar());
+
+        
+        // Decoro novamente com rede de ping-pong para mostrar que o padrão tá
+        // funcionando e que ele sempre decorará, mesmo que repetidamente
         mesa = new MesaComRedePingPong(mesa);
         System.out.println(mesa.decorar());
     }
